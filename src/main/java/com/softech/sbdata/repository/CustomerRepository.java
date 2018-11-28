2e57e542022b65e7126528504d27833ab1a26be8
Huỳnh Nguyễn Viết <nguyenviethuynh1996.vih@gmail.com>
2018-11-28 15:45:32 +0700

    create page

--- src/main/java/com/softech/sbdata/repository/CustomerRepository.java
+++ src/main/java/com/softech/sbdata/repository/CustomerRepository.java
@@ -1,8 +1,16 @@
 package com.softech.sbdata.repository;
 
 import com.softech.sbdata.entities.Customer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+    @Query("SELECT e from Customer e")
+    Page<Customer> findCustomers(Pageable pageable);
 
 }
